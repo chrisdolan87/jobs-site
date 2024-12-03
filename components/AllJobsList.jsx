a3084bac2e21241ab705b5c74633b149d61bc738
Chris Dolan <d7oln@hotmail.com>
2024-12-03 19:00:28 +0000

    cd

--- components/AllJobsList.jsx
+++ components/AllJobsList.jsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 const getJobs = async () => {
   try {
-    const res = await fetch(`https://it-cw2-jobs-site.vercel.app/api/jobs`, {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/jobs`, {
       cache: "no-store",
     });
 
